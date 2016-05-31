c5a9913b4a6591b487e1e32c249c139aa2c100bb
Vladimir <edoomik@gmail.com>
2016-05-31 13:39:46 +0300

    added 'control' directive to handle key presses

---
+++ scripts/game/control.directive.js
@@ -0,0 +1,55 @@
+(function() {
+  'use strict';
+
+  angular.module('puzzle15').directive('control', control);
+  control.$inject = [];
+
+  function control() {
+    var directive = {
+      restrict: 'E',
+      scope: {
+        key: '@',
+        action: '&'
+      },
+      link: function(scope) {
+        var controls = {
+          'ArrowDown': 40,
+          'ArrowUp': 38,
+          'ArrowLeft': 37,
+          'ArrowRight': 39
+        };
+
+        var controlsReversed = reverse(controls);
+
+        var onKeyPress = function(event) {
+          if (controlsReversed.hasOwnProperty(event.keyCode) &&
+            scope.key === controlsReversed[event.keyCode]) {
+            event.preventDefault();
+            scope.action();
+          }
+        };
+
+        document.addEventListener('keydown', onKeyPress);
+
+        scope.$on('$destroy', function() {
+          document.removeEventListener('keydown', onKeyPress);
+        });
+      }
+    };
+
+    return directive;
+  }
+
+  function reverse(obj) {
+    var keys = Object.keys(obj),
+      inverted = {},
+      val;
+
+    keys.forEach(function(key) {
+      val = obj[key];
+      inverted[val] = key;
+    });
+
+    return inverted;
+  }
+})();
